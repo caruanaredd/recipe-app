460b353c311cb0f6cabc807735f604fe1046bc4e
Redd <redd.caruana@mcast.edu.mt>
2020-04-30 12:38:18 +0200

    Data Management
    Added Service function to retrieve categories and recipes.
    
    Added information to the home page.

--- recipe-cart/src/app/recipe/recipe-home/recipe-home.page.ts
+++ recipe-cart/src/app/recipe/recipe-home/recipe-home.page.ts
@@ -9,6 +9,16 @@ import { RecipeService } from 'src/app/services/recipe.service';
 export class RecipeHomePage implements OnInit
 {
     /**
+     * The categories available to this interface.
+     */
+    public categories: any[] = [];
+
+    /**
+     * The recipes available to this interface.
+     */
+    public recipes: any[] = [];
+
+    /**
      * The category slider options.
      */
     public categorySlideOpts = {
@@ -26,10 +36,17 @@ export class RecipeHomePage implements OnInit
         public recipeService: RecipeService
     ) { }
 
-    ngOnInit()
+    // use async whenever there is dynamic data that is loaded into the page/app.
+    async ngOnInit()
     {
         console.log("Starting to load...");
-        this.recipeService.preload();
+
+        // we'll wait for the function to complete before the app is shown.
+        await this.recipeService.preload();
+
+        this.categories = this.recipeService.getCategories();
+        this.recipes = this.recipeService.getRecipes(true, 5);
+        
         console.log("Finished.");
     }
 
--- recipe-cart/src/app/services/recipe.service.ts
+++ recipe-cart/src/app/services/recipe.service.ts
@@ -9,6 +9,20 @@ import * as xml2js from 'xml2js';
 })
 export class RecipeService
 {
+    /**
+     * The data available to this service.
+     * By default, it is empty.
+     */
+    private _data: any = null;
+
+    /**
+     * True if the service data is populated, and false if not.
+     */
+    public get dataIsLoaded(): boolean
+    {
+        return this._data != null;
+    }
+
     // we need to inject HttpClient into this class
     // so that we can use the XML plugin.
     constructor(
@@ -17,29 +31,132 @@ export class RecipeService
 
     /**
      * Loads the data inside the app's XML file into this service.
+     * This is an asynchronous function that can be queued with other code.
      */
-    public preload()
+    public async preload()
     {
         // this.http.get is a function that will take time to process.
         // in the meantime, the app will continue working with no data.
         // subscribing to this process will make sure I can use the data when it's loaded.
-        this.http.get('/assets/data.xml', {
+        await this.http.get('/assets/data.xml', {
             headers: new HttpHeaders()
+                // Content-Type is how the file will be read (in this case, as text)
                 .set('Content-Type', 'text/xml')
+
+                // We're asking to GET/download the file.
                 .append('Access-Control-Allow-Methods', 'GET')
+
+                // Allows communication with any source on any website.
                 .append('Access-Control-Allow-Origin', '*')
+
+                // These are permission enabling options.
                 .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method"),
+            
+            // If not text, the content will be interpreted as JSON.
             responseType: 'text'
-        }).subscribe(
-            // What happens when the connection has been successful.
+        })
+        // Promises are the same as observables, however their structure
+        // can be more flexible to work with.
+        .toPromise()
+        // When the promise finishes, then what?
+        .then(
+            // What happens when the connection is successful.
             (content: string) => {
-                console.log(content);
+                this.parseData(content);
             },
 
-            // What happens when there is an error.
+            // What happens when the connection was unsuccessful.
             error => {
                 console.warn(error);
             }
         );
+    }
+
+    /**
+     * Converts XML data into JSON.
+     * @param content The XML data as a string.
+     */
+    private parseData(content: string)
+    {
+        xml2js.parseString(content, {
+            attrkey: 'attr',
+            trim: true
+        }, (error, result) => {
+            // if there's an error, we'll want to stop moving forward.
+            if (error != null)
+            {
+                console.warn(error);
+                return;
+            }
+
+            // populate the variable in this service to prevent further loading
+            // in different pages.
+            this._data = result.data;
+            console.log(this._data);
+        });
+    }
+
+    /**
+     * Creates an array of categories from the data list.
+     */
+    public getCategories()
+    {
+        // this._data contains 2 properties:
+        // attr -> the main tag attributes
+        // category -> an array of categories
+        
+        // if I want the category name, I need to navigate to:
+        // this._data.category[0].attr.name
+
+        // map will loop through the entire array of categories and process them
+        // identically and individually.
+        // in this example, we want to retrieve the name of our categories.
+        return this._data.category.map(c => c.attr.name);
+    }
+
+    /**
+     * Creates an array of recipes from the data list.
+     * @param shuffle Will randomize the order if true.
+     * @param count Will limit the number of recipes if a value is given.
+     */
+    public getRecipes(shuffle: boolean = false, count?: number)
+    {
+        // a category contains 2 properties:
+        // attr -> the name/data of this category
+        // recipe -> an array of recipes
+        
+        // if I want to access the recipe name and url, I need to navigate into:
+        // this._data.category[0].recipe[0].attr.name
+
+        var recipes = [];
+
+        // this creates a nested array of recipes
+        // but I want the recipes as a single array instead.
+        // forEach will loop through every item in the category
+        // and add it into the array above.
+        this._data.category.map(
+            c => c.recipe.forEach(
+                r => recipes.push(r)
+            )
+        );
+
+        // if we want to shuffle the array,
+        // we'll do it before we give back the variable.
+        if (shuffle)
+        {
+            // to sort in a random order
+            // calculate a number from 0 - 1 (which might include 0.5, 0.3, etc)
+            // when we subtract 0.5 from this number, it will randomize the order of the array.
+            recipes = recipes.sort(() => 0.5 - Math.random());
+        }
+
+        // if we want to reduce the number of items,
+        // we can do it via slice.
+        if (count)
+        {
+            recipes = recipes.slice(0, count);
+        }
+
+        return recipes;
     }
 }
